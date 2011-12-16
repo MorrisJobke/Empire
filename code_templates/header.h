504cfb7f2f458d5fca006fd8118e2875a8774d55
sighter laptop <sighter@resource-dnb.de>
2011-12-16 13:00:01 +0100

    added some nice vim templates

---
+++ code_templates/header.h
@@ -0,0 +1,15 @@
+
+/* <+FILE NAME+>
+ * @Author:      <+AUTHOR+> (<+EMAIL+>)
+ * @License:     <+LICENSE+>
+ * @Created:     <+DATE+>.
+ * @Editor:      Vim
+ */
+
+#ifndef <+FILE NAME:u:s/\./_/+>
+#define <+FILE NAME:u:s/\./_/+>
+
+
+
+
+#endif
