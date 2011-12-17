70ab4dc1e7ead67f36eb6d7cc6efc41170f3b0f9
kabum <uu.kabum@gmail.com>
2011-12-17 15:36:31 +0100

    Re-enable tests

--- src/tests/PropertyIntegerTests.cpp
+++ src/tests/PropertyIntegerTests.cpp
@@ -7,27 +7,25 @@
 BOOST_AUTO_TEST_CASE(typeIsInteger)
 {
     BOOST_CHECK(42 == 42);
-    BOOST_CHECK(41 == 42);
-    PropertyInteger property = new PropertyInteger(42);
+    PropertyInteger* property = new PropertyInteger(42);
 
-//     BOOST_CHECK(isdigit(property.GetValue()));
-//     BOOST_CHECK(property.GetValue() == 42);
+    BOOST_CHECK(property->GetValue() == 42);
 }
 
 BOOST_AUTO_TEST_CASE(testOverloadedConstructors)
 {
-    // PropertyInteger property = PropertyInteger(42);
-    // BOOST_CHECK(property.GetValue() == 42);
-    // BOOST_CHECK(property.GetKey() == NULL);
-    // BOOST_CHECK(property.GetDescription() == NULL);
+    PropertyInteger* property = new PropertyInteger(42);
+    BOOST_CHECK(property->GetValue() == 42);
+    BOOST_CHECK(property->GetKey() == "");
+    BOOST_CHECK(property->GetDescription() == "");
 
-    // property = PropertyInteger("key", 42);
-    // BOOST_CHECK(property.GetValue() == 42);
-    // BOOST_CHECK(property.GetKey() == "key");
-    // BOOST_CHECK(property.GetDescription() == NULL);
+    property = new PropertyInteger("key", 42);
+    BOOST_CHECK(property->GetValue() == 42);
+    BOOST_CHECK(property->GetKey() == "key");
+    BOOST_CHECK(property->GetDescription() == "");
 
-    // property = PropertyInteger("key", "i am a property", 42);
-    // BOOST_CHECK(property.GetValue() == 42);
-    // BOOST_CHECK(property.GetKey() == "key");
-    // BOOST_CHECK(property.GetDescription() == "i am a property");
+    property = new PropertyInteger("key", "i am a property", 42);
+    BOOST_CHECK(property->GetValue() == 42);
+    BOOST_CHECK(property->GetKey() == "key");
+    BOOST_CHECK(property->GetDescription() == "i am a property");
 }
