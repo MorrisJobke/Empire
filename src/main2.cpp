d46862c92a4601d13fd6a419bf93d3bbf4a4a08d
kabum <uu.kabum@gmail.com>
2011-12-18 23:16:39 +0100

    remove incorrect located code

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,23 +1,6 @@
 #include "properties/PropertyInteger.h"
-#include "collections/Collection.h"
-#include "collections/Collectionlist.h"
 #include <string>
 #include <iostream>
-
-struct Collectionlist *Collectionlist()
-{
-    Collectionlist* mCollectionlist = new Collectionlist;
-    //fill Collectionlist with all Collections
-    return mCollectionlist;
-}
-
-struct Collectionlist *Collectionlist(string const& key)
-{
-    Collectionlist* mCollectionlist = new Collectionlist;
-    //fill Collectionlist with the Collection with the key
-    return mCollectionlist;
-}
-
 
 int main() {
     PropertyInteger intProp = PropertyInteger("test", 42);
