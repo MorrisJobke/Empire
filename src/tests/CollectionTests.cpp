07640019f4ead5e1b2468ca15b5df21dc08a3197
kabum <uu.kabum@gmail.com>
2011-12-18 16:45:41 +0100

    [Tests] Collection tests

--- src/tests/CollectionTests.cpp
+++ src/tests/CollectionTests.cpp
@@ -4,6 +4,8 @@
 #endif
 #include <boost/test/unit_test.hpp>
 #include "../collections/Collection.h"
+#include "../properties/PropertyInteger.h"
+#include "../properties/PropertyFloat.h"
 
 BOOST_AUTO_TEST_SUITE(CollectionTests_Suite)
 
@@ -13,16 +15,12 @@ BOOST_AUTO_TEST_CASE(testOverloadedConstructors)
     BOOST_CHECK(coll->GetKey() == "key");
     BOOST_CHECK(coll->GetPropertylist() == NULL);
 
-    PropertyInteger* property = new PropertyInteger("intkey", 42);
-    PropertyFloat* property2 = new PropertyFloat("floatkey", "i am a property", 3.14159265);
-    PropertyList* proplist = new Propertylist;
-    proplist->prop = property;
-    proplist->next = new Propertylist;
-    proplist->next->prop = property2;
-    proplist->next->next = NULL;    
-    coll = new Collection("key", proplist);
+    coll = new Collection("key");
+    PropertyInteger property ("intkey", 42);
+    coll->AddProperty(property);
+    PropertyFloat property2 ("floatkey", "i am a property", 3.14159265);
+    coll->AddProperty(property2);
     BOOST_CHECK(coll->GetKey() == "key");
-    BOOST_CHECK(coll->GetPropertylist() == proplist);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
