9caaa708b38604ff7b6dd8cca71d03284f2fc4dc
sighter <sighter@resource-dnb.de>
2012-01-09 00:56:05 +0100

    addded command test dummy

---
+++ src/tests/CommandTests.cpp
@@ -0,0 +1,33 @@
+
+/* CommandTests.cpp
+ * @Author:      The Sighter (sighter@resource-dnb.de)
+ * @License:     GPL
+ * @Created:     2012-01-08.
+ * @Editor:      Vim
+ */
+
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE PropertyInteger
+#endif
+
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+
+#include <boost/test/unit_test.hpp>
+
+#include "../GenProperty.h"
+#include "../Filesystem.h"
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(CommandTestSuite)
+
+BOOST_AUTO_TEST_CASE(InitTest)
+{
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
