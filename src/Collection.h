1b8041c35f5e4bbbe996e5010bddec1a8e5da600
sighter <sighter@resource-dnb.de>
2012-01-17 17:14:57 +0100

    [Collection] some declarations

---
+++ src/Collection.h
@@ -0,0 +1,37 @@
+
+/* Collection.h
+ * @Author:      The Sighter (sighter@resource-dnb.de)
+ * @License:     GPL
+ * @Created:     2012-01-17.
+ * @Editor:      Vim
+ */
+
+#ifndef COLLECTION_H
+#define COLLECTION_H
+
+#include <iostream>
+#include <list>
+
+
+/** collection class
+ */
+class coll
+{
+    std::list<std::list<GenPropertyBase*>> mPropList;
+
+    public:
+
+    coll();
+    coll(std::string const& rPath);
+
+    ~coll();
+
+    AddRow(std::list<GenPropertyBase*> const&);
+
+    std::list<std::list<GenPropertyBase*>> GetList() const;
+
+    WriteToDir(std::string const& rPath)
+};
+
+
+#endif
