4a0f1a3077af605eeba60cb9740c10f3157bff67
kabum <uu.kabum@gmail.com>
2012-01-18 22:25:35 +0100

    [FunctionType] initial declaration

---
+++ src/FunctionType.h
@@ -0,0 +1,30 @@
+/* FunctionType.h
+ * @Author:      kabum
+ * @License:     GPL
+ * @Created:     2012-01-18
+ * @Editor:      Sublime Text 2
+ */
+
+#ifndef FUNCTIONTYPE_H
+#define FUNCTIONTYPE_H
+
+/** Lua Context
+ */
+class FunctionType
+{
+public:
+    FunctionType();
+    ~FunctionType();
+    /** copy constructor
+     */
+    FunctionType(FunctionType& rOrigin);
+
+    /** assignment operator
+     */
+    FunctionType& operator=(FunctionType& rRight);
+
+    void ToOutStream(std::ostream& rOut);
+};
+
+
+#endif
