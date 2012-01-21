a6523550621a73df4aaa43145990e8ea5dd4e19e
kabum <uu.kabum@gmail.com>
2012-01-21 16:58:27 +0100

    [TemplateTests] test for previous commit

--- src/tests/TemplateTests.cpp
+++ src/tests/TemplateTests.cpp
@@ -143,4 +143,79 @@ BOOST_AUTO_TEST_CASE(TemplateCollectionTest)
     remove("ingredients");
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+BOOST_AUTO_TEST_CASE(TemplateCollectionAndFunctionTest)
+{
+    GenProperty<string> ingredient1_ingredient("Midori Melon Liqueur", "ingredient");
+    GenProperty<double> ingredient1_oz(1, "oz");
+    GenProperty<double> ingredient1_priceperoz(1.67, "priceperoz");
+    GenProperty<string> ingredient2_ingredient("Blue Curacao", "ingredient");
+    GenProperty<double> ingredient2_oz(3, "oz");
+    GenProperty<double> ingredient2_priceperoz(2.03, "priceperoz");
+    GenProperty<string> ingredient3_ingredient("Lemonade", "ingredient");
+    GenProperty<double> ingredient3_oz(0.5, "oz");
+    GenProperty<double> ingredient3_priceperoz(7.84, "priceperoz");
+
+    string map = "return oz * priceperoz;";
+    string reduce = "return lhs + rhs;";
+    list<string> functions;
+    functions.push_back(map);
+    functions.push_back(reduce);
+    FunctionType func(functions);
+    GenProperty<FunctionType> price(func, "price");
+
+    std::list<GenPropertyBase*> ingredient1;
+    ingredient1.push_back(&ingredient1_ingredient);
+    ingredient1.push_back(&ingredient1_oz);
+    ingredient1.push_back(&ingredient1_priceperoz);
+
+    std::list<GenPropertyBase*> ingredient2;
+    ingredient2.push_back(&ingredient2_ingredient);
+    ingredient2.push_back(&ingredient2_oz);
+    ingredient2.push_back(&ingredient2_priceperoz);
+
+    std::list<GenPropertyBase*> ingredient3;
+    ingredient3.push_back(&ingredient3_ingredient);
+    ingredient3.push_back(&ingredient3_oz);
+    ingredient3.push_back(&ingredient3_priceperoz);
+
+    Coll ingredients_coll("ingredients");
+    ingredients_coll.Declare(ingredient1);
+    ingredients_coll.AddRow(ingredient2);
+    ingredients_coll.AddRow(ingredient3);
+
+    GenProperty<Coll> ingredients(ingredients_coll, "ingredients");
+
+    SimpleTemplate* tmpl = new SimpleTemplate();
+    tmpl->AddProperty(&ingredients);
+    tmpl->AddProperty(&price);
+
+    string input = "Nerds Recipe Vol.2\n\n@ingredients{\n@oz oz @ingredient @price\n}";
+    string output;
+    tmpl->ParseString(input, output);
+
+    std::cout << "output: " << output << endl;
+
+    BOOST_CHECK(output == "Nerds Recipe Vol.2\n\n1 oz Midori Melon Liqueur 1.67\n3 oz Blue Curacao 6.09\n0.5 oz Lemonade 3.92\n");
+
+    remove("ingredients/0/ingredient");
+    remove("ingredients/0/oz");
+    remove("ingredients/0/priceperoz");
+    remove("ingredients/0");
+    remove("ingredients/1/ingredient");
+    remove("ingredients/1/oz");
+    remove("ingredients/1/priceperoz");
+    remove("ingredients/1");
+    remove("ingredients/2/ingredient");
+    remove("ingredients/2/oz");
+    remove("ingredients/2/priceperoz");
+    remove("ingredients/2");
+    remove("ingredients/3/ingredient");
+    remove("ingredients/3/oz");
+    remove("ingredients/3/priceperoz");
+    remove("ingredients/3");
+    remove("ingredients");
+}
+
 BOOST_AUTO_TEST_SUITE_END()
