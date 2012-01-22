c5d1df4edc39794ec2a781973c1164c5fc57a736
Robert Schönherr <rbs@informatik.tu-chemnitz.de>
2012-01-22 17:28:31 +0100

    [SyntaxParser] Added

---
+++ src/SyntaxParser.cc
@@ -0,0 +1,71 @@
+
+/* SyntaxParser.cc
+ * @Author:      rbs (rbs@hrz.tu-chemnitz.de)
+ * @License:     GPL
+ * @Created:     2012-01-22
+ * @Editor:      Sublime Text 2
+ */
+
+#include "SyntaxParser.h"
+
+//////////////////////////////////////////////////////////////////////////////
+namespace SyntaxParser
+{
+    /** Add Command
+     *
+     * @param argc count of arguments
+     * @param argv arguments
+     */
+    void add(int argc, char* argv[])
+    {
+        
+    }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+    
+    /** modify command
+     *
+     * @param argc count of arguments
+     * @param argv arguments
+     */
+    void modify(int argc, char* argv[])
+    {
+        
+    }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+    /** add command
+     *
+     * @param argc count of arguments
+     * @param argv arguments
+     */
+    void show(int argc, char* argv[])
+    {
+        
+    }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+    /** init command
+     *
+     * @param argc count of arguments
+     * @param argv arguments
+     */
+    void init(int argc, char* argv[])
+    {
+        
+    }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+    
+    /** render command
+     *
+     * @param argc count of arguments
+     * @param argv arguments
+     */
+    void render(int argc, char* argv[])
+    {
+        
+    }
+}
---
+++ src/SyntaxParser.h
@@ -0,0 +1,24 @@
+
+/* SyntaxParser.h
+ * @Author:      rbs (rbs@hrz.tu-chemnitz.de)
+ * @License:     GPL
+ * @Created:     2012-01-22
+ * @Editor:      Sublime Text 2
+ */
+
+#ifndef SYNTAXPARSER_H
+#define SYNTAXPARSER_H
+
+
+namespace SyntaxParser
+{
+    void add(int argc, char* argv[]);
+    void modify(int argc, char* argv[]);
+    void show(int argc, char* argv[]);
+    void init(int argc, char* argv[]);
+    void render(int argc, char* argv[]);
+
+}
+
+
+#endif
